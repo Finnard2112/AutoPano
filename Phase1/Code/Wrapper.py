93044a2876be015caeac7c1ce31f1f7dde061b40
Finnard123 <finnard2112@gmail.com>
2026-02-14 17:04:55 -0500

    Detect corners with Harris

--- Phase1/Code/Wrapper.py
+++ Phase1/Code/Wrapper.py
@@ -16,29 +16,47 @@ University of Maryland, College Park
 """
 
 # Code starts here:
-
+import os
 import numpy as np
-import cv2
+import cv2 as cv
+from matplotlib import pyplot as plt
+import argparse
 # Add any python libraries here
 
 
 
 def main():
 	# Add any Command Line arguments here
-    # Parser = argparse.ArgumentParser()
-    # Parser.add_argument('--NumFeatures', default=100, help='Number of best features to extract from each image, Default:100')
-    
-    # Args = Parser.parse_args()
-    # NumFeatures = Args.NumFeatures
+	Parser = argparse.ArgumentParser()
+	Parser.add_argument('--imgpath', default="../Data/Train/Set1", help='Include the image path')
+	Args = Parser.parse_args()
+	imgpath = Args.imgpath
 
 	"""
 	Read a set of images for Panorama stitching
+
 	"""
+	files = [os.path.join(imgpath, f) for f in os.listdir(imgpath)]
+	images = [cv.imread(file) for file in files]
 
 	"""
 	Corner Detection
 	Save Corner detection output as corners.png
 	"""
+
+	for img, file in zip(images, files):
+
+		# Detect corners
+		gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+		gray = np.float32(gray)
+		dst = cv.cornerHarris(gray, blockSize=2, ksize=3, k=0.04)
+		dst = cv.dilate(dst, None)
+		img[dst > 0.01 * dst.max()] = [0, 0, 255]
+
+		# Save image
+		filename = os.path.basename(file)
+		save_path = os.path.join("../Output", f"harris_{filename}")
+		cv.imwrite(save_path, img)
 
 	"""
 	Perform ANMS: Adaptive Non-Maximal Suppression
