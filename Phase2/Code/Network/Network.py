be131079a2162ee23b2d4d3d37f048575242b70f
apslying <hw987@umd.edu>
2026-02-22 23:46:59 -0500

    Data Generation and Supervised Approach

---
+++ Phase2/Code/Network/Network.py
@@ -0,0 +1,69 @@
+"""
+CMSC733 Spring 2019: Classical and Deep Learning Approaches for
+Geometric Computer Vision
+Homework 0: Alohomora: Phase 2 Starter Code
+
+
+Author(s):
+Nitin J. Sanket (nitinsan@terpmail.umd.edu)
+PhD Candidate in Computer Science,
+University of Maryland, College Park
+"""
+
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+import sys
+import numpy as np
+# Don't generate pyc codes
+sys.dont_write_bytecode = True
+
+def HomographyModel(Img, ImageSize, MiniBatchSize):
+    """
+    Inputs: 
+    Img is a MiniBatch of the current image
+    ImageSize - Size of the Image (128x128x6)
+    Outputs:
+    prLogits - logits output of the network
+    prSoftMax - softmax output of the network
+    """
+    
+    # Conv Block 1
+    conv1_1 = tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same', name='conv1_1')(Img)
+    # conv1_1 = tf.keras.layers.BatchNormalization(name='bn1_1')(conv1_1)
+    conv1_2 = tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same', name='conv1_2')(conv1_1)
+    # conv1_2 = tf.keras.layers.BatchNormalization(name='bn1_2')(conv1_2)
+    pool1 = tf.keras.layers.MaxPooling2D(2, 2, name='pool1')(conv1_2)
+
+    # Conv Block 2
+    conv2_1 = tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same', name='conv2_1')(pool1)
+    # conv2_1 = tf.keras.layers.BatchNormalization(name='bn2_1')(conv2_1)
+    conv2_2 = tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same', name='conv2_2')(conv2_1)
+    # conv2_2 = tf.keras.layers.BatchNormalization(name='bn2_2')(conv2_2)
+    pool2 = tf.keras.layers.MaxPooling2D(2, 2, name='pool2')(conv2_2)
+
+    # Conv Block 3
+    conv3_1 = tf.keras.layers.Conv2D(128, 3, activation='relu', padding='same', name='conv3_1')(pool2)
+    # conv3_1 = tf.keras.layers.BatchNormalization(name='bn3_1')(conv3_1)
+    conv3_2 = tf.keras.layers.Conv2D(128, 3, activation='relu', padding='same', name='conv3_2')(conv3_1)
+    # conv3_2 = tf.keras.layers.BatchNormalization(name='bn3_2')(conv3_2)
+    pool3 = tf.keras.layers.MaxPooling2D(2, 2, name='pool3')(conv3_2)
+
+    # Conv Block 4
+    conv4_1 = tf.keras.layers.Conv2D(128, 3, activation='relu', padding='same', name='conv4_1')(pool3)
+    # conv4_1 = tf.keras.layers.BatchNormalization(name='bn4_1')(conv4_1)
+    conv4_2 = tf.keras.layers.Conv2D(128, 3, activation='relu', padding='same', name='conv4_2')(conv4_1)
+    # conv4_2 = tf.keras.layers.BatchNormalization(name='bn4_2')(conv4_2)
+
+    # Fully Connected Layers
+    flat = tf.keras.layers.Flatten(name='flatten')(conv4_2)
+    fc1 = tf.keras.layers.Dense(1024, activation='relu', name='fc1')(flat)
+    drop1 = tf.keras.layers.Dropout(0.5, name='dropout1')(fc1)
+    fc2 = tf.keras.layers.Dense(1024, activation='relu', name='fc2')(drop1)
+    drop2 = tf.keras.layers.Dropout(0.5, name='dropout2')(fc2)
+
+    # Output Layer (8 homography values)
+    prLogits = tf.keras.layers.Dense(8, activation=None, name='output')(drop2)
+    prSoftMax = prLogits
+    
+    return prLogits, prSoftMax
+
---
+++ Phase2/Code/Wrapper.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+
+"""
+CMSC733 Spring 2019: Classical and Deep Learning Approaches for
+Geometric Computer Vision
+Project1: MyAutoPano: Phase 1 Starter Code
+
+Author(s): 
+Chahat Deep Singh (chahat@terpmail.umd.edu) 
+PhD Student in Computer Science,
+University of Maryland, College Park
+
+Nitin J. Sanket (nitinsan@terpmail.umd.edu)
+PhD Candidate in Computer Science,
+University of Maryland, College Park
+"""
+
+# Code starts here:
+
+import numpy as np
+import cv2 as cv
+# Add any python libraries here
+
+
+
+def main():
+	# Add any Command Line arguments here
+	# Parser = argparse.ArgumentParser()
+	# Parser.add_argument('--NumFeatures', default=100, help='Number of best features to extract from each image, Default:100')
+	
+	# Args = Parser.parse_args()
+	# NumFeatures = Args.NumFeatures
+
+	"""
+	Read a set of images for Panorama stitching
+	"""
+	N = 5000
+	#read the first N images
+	images = [cv.imread(f'/nfshomes/hw987/cmsc733_Vision/YourDirectoryID_p1/Phase2/Data/Train/{i}.jpg') for i in range(1, N+1)]
+
+	labels_file = open('./TxtFiles/LabelsTrain.txt', 'w')
+	dirnames_file = open('./TxtFiles/DirNamesTrain.txt', 'w')
+
+	for i, image in enumerate(images):
+		# should be (426, 640, 3)
+		height, width = image.shape[:2]
+
+		#maximum possible perturbation
+		rho = 32
+		#patch size
+		p_w = 128
+		p_h = 128
+
+		
+		#TODO: make sure corners not too close to the edges, or else the perturbed corners will be too close to the edges
+
+		# 3. Generate a random top-left corner (x, y)
+		x1 = np.random.randint(rho, width - p_w - rho)
+		y1 = np.random.randint(rho, height - p_h - rho)
+
+		# 4. Calculate coordinates for all 4 corners
+		x2, y2 = x1 + p_w, y1 + p_h
+		
+		corners = np.array([[x1, y1], [x2, y1], [x2, y2], [x1, y2]], dtype=np.int32)
+
+		patch_original = image[corners[0, 1]:corners[2, 1], corners[0, 0]:corners[2, 0]]
+
+		corners_perturbed = np.array([[x1 + np.random.randint(-rho, rho), y1 + np.random.randint(-rho, rho)], [x2 + np.random.randint(-rho, rho), y1 + np.random.randint(-rho, rho)], [x2 + np.random.randint(-rho, rho), y2 + np.random.randint(-rho, rho)], [x1 + np.random.randint(-rho, rho), y2 + np.random.randint(-rho, rho)]], dtype=np.int32)
+
+		patch_perturbed = image[corners_perturbed[0, 1]:corners_perturbed[2, 1], corners_perturbed[0, 0]:corners_perturbed[2, 0]]
+
+
+		homography = cv.getPerspectiveTransform(corners_perturbed.astype(np.float32), corners.astype(np.float32))
+		#this is the homography that maps the perturbed patch to the original patch
+		warped_image = cv.warpPerspective(image, homography, (width, height))
+		warped_patch = warped_image[corners[0, 1]:corners[2, 1], corners[0, 0]:corners[2, 0]]
+
+		#save patch_original and warped_patch
+		cv.imwrite(f'../Data/Patches/Original/{i+1}.jpg', patch_original)
+		cv.imwrite(f'../Data/Patches/Warped/{i+1}.jpg', warped_patch)
+
+
+
+
+		#save patch_original and warped_patch
+		corner_displacement = corners_perturbed - corners
+		# stacked_images = np.concatenate((patch_original, warped_patch), axis=2)
+		
+		
+		label_values = corner_displacement.flatten()
+		labels_file.write(' '.join(map(str, label_values)) + '\n')
+		
+		dirnames_file.write(f'{i+1}\n')
+
+	labels_file.close()
+	dirnames_file.close()
+	
+	"""
+	Obtain Homography using Deep Learning Model (Supervised and Unsupervised)
+	"""
+	
+	"""
+	Image Warping + Blending
+	Save Panorama output as mypano.png
+	"""
+
+	
+if __name__ == '__main__':
+	main()
+ 
